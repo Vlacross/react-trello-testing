bfde42890abda9af4a5f948921ed8ad264aec070
Tim Vlasuk <timvlasuk@gmail.com>
2021-05-18 10:04:04 -0700

    Moved to unofficial Enzyme Adapter to allow Enzyme to work with React 17; installed react as dep

--- src/components/add-form.test.js
+++ src/components/add-form.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
+
 import AddForm from './add-form';
 
 describe('<AddForm />', () => {
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+/* 
+Enzyme not yet compatible with React 17 so using unofficial Adapter
+https://github.com/enzymejs/enzyme/issues/2429
+// import Adapter from 'enzyme-adapter-react-16';
+*/
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter: new Adapter()});
